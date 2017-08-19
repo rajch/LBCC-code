af24f9da2f8086e934acaed5e9c338401d8f71c6
Raj Chaudhuri <rajch@hotmail.com>
2017-08-19 16:30:54 +0530

    Start of Chapter 3

---
+++ Chapter3/CodeGen.vb
@@ -0,0 +1,132 @@
+Option Strict On
+Option Explicit On
+
+
+Imports System
+Imports System.Reflection
+Imports System.Reflection.Emit
+
+
+Public Class CodeGen
+	Private m_ILGen As ILGenerator
+	Private m_producedAssembly As AssemblyBuilder
+	Private m_producedmodule As ModuleBuilder
+	Private m_producedtype As TypeBuilder
+	Private m_producedmethod As MethodBuilder
+	Private m_SaveToFile As String
+
+	Public Sub EmitNumber(ByVal num As Integer)
+		m_ILGen.Emit(OpCodes.Ldc_I4, num)
+	End Sub
+
+	Public Sub EmitString(ByVal str As String)
+		m_ILGen.Emit(OpCodes.Ldstr, str)
+	End Sub
+
+	Public Sub EmitAdd()
+		m_ILGen.Emit(OpCodes.Add)
+	End Sub
+
+	Public Sub EmitSubtract()
+		m_ILGen.Emit(OpCodes.Sub)
+	End Sub
+
+	Public Sub EmitMultiply()
+		m_ILGen.Emit(OpCodes.Mul)
+	End Sub
+
+	Public Sub EmitDivide()
+		m_ILGen.Emit(OpCodes.Div)
+	End Sub
+
+	Public Sub EmitWriteLine()
+		Dim inttype As Type = Type.GetType("System.Int32")
+		Dim consoletype As Type = Type.GetType("System.Console")
+		Dim paramtypes() As Type = {inttype}
+
+		m_ILGen.Emit( _
+			OpCodes.Call, _
+			consoletype.GetMethod( _
+				"WriteLine", paramtypes _
+			) _
+		)
+	End Sub
+
+	Public Sub EmitWriteLineString()
+		Dim strtype As Type = Type.GetType("System.String")
+		Dim consoletype As Type = Type.GetType("System.Console")
+		Dim paramtypes() As Type = {strtype}
+
+		m_ILGen.Emit( _
+			OpCodes.Call, _
+			consoletype.GetMethod( _
+				"WriteLine", paramtypes _
+			) _
+		)
+	End Sub
+
+	Public Sub New(ByVal FileName As String)
+		m_SaveToFile = FileName
+
+		' Compiling a CLR language produces an assembly.
+		' An assembly has one or more modules.
+		' Each module has one or more types:
+		' (structures or classes)
+		' Each type has one or more methods.
+		' Methods are where actual code resides.
+		' Create an assembly called "MainAssembly".
+
+		Dim an As New AssemblyName
+		an.Name = "MainAssembly"
+
+		m_producedAssembly = AppDomain.CurrentDomain.DefineDynamicAssembly( _
+							an, AssemblyBuilderAccess.Save _
+		)
+
+		' In MainAssembly, create a module called
+		' "MainModule".
+		m_producedmodule = m_producedAssembly.DefineDynamicModule( _
+							"MainModule", FileName, False
+		)
+
+		' In MainModule, create a class called
+		' "MainClass".
+		m_producedtype = m_producedmodule.DefineType("MainClass")
+
+		' In MainClass, create a Shared (static) method
+		' with Public scope, called "MainMethod".
+		m_producedmethod = m_producedtype.DefineMethod( _
+						"MainMethod", _
+						MethodAttributes.Public Or MethodAttributes.Static, _
+						Nothing, _
+						Nothing _
+		)
+
+		' All IL code that we produce will be contained
+		' in MainMethod.
+		m_ILGen = m_producedmethod.GetILGenerator
+
+	End Sub
+
+	Public Sub Save()
+		' Emit a RETurn opcode, which is the last
+		' opcode for any method
+		m_ILGen.Emit(OpCodes.Ret)
+
+		' Actually create the type in the module
+		m_producedtype.CreateType()
+
+		' Specify that when the produced assembly
+		' is run, execution will start from
+		' the produced method (MainMethod). Also, the
+		' produced assembly will be a console
+		' application.
+		m_producedAssembly.SetEntryPoint( _
+			m_producedmethod, _
+			PEFileKinds.ConsoleApplication
+		)
+
+		m_producedAssembly.Save(m_SaveToFile)
+	End Sub
+
+End Class
---
+++ Chapter3/Parser.vb
@@ -0,0 +1,350 @@
+Imports System
+Imports System.IO
+Imports System.Text
+
+Public Class Parser
+
+#Region "Fields"
+    Private m_InputStream As TextReader
+    Private m_Gen As CodeGen
+
+    ' The current line being translated
+    Private m_ThisLine As String
+    Private m_LineLength As Integer = 0
+
+    ' The position of the line being translated
+    Private m_linePos As Integer = 0
+    ' The character position currently being looked at
+    Private m_CharPos As Integer = 0
+
+    ' The current program element
+    Private m_CurrentTokenBldr As StringBuilder
+#End Region
+
+#Region "Helper Functions"
+    Private Function CreateError( _
+        ByVal errorcode As Integer, _
+        ByVal errorDescription As String _
+        ) As ParseStatus
+
+        Dim result As ParseStatus
+        Dim message As String
+
+        Select Case errorcode
+            Case 0
+                message = "Ok."
+            Case 1
+                message = String.Format( _
+                            "Expected {0}", _
+                            errorDescription _
+                )
+        End Select
+
+        result = New ParseStatus(errorcode, _
+                    message, _
+                    m_CharPos + 1, _
+                    m_linePos)
+
+
+        Return result
+    End Function
+#End Region
+
+#Region "Recognizers"
+    Private Function IsNumeric(ByVal c As Char) As Boolean
+        Dim result As Boolean
+
+        If Char.IsDigit(c) Then
+            ' Use the CLR's built-in digit recognition
+            result = True
+        ElseIf "+-".IndexOf(c) <> -1 And _
+            TokenLength = 0 Then
+            ' If the symbol being cheked is + or -
+            ' AND we are at the START of the current
+            ' token
+            result = True
+        Else
+            result = False
+        End If
+
+        Return result
+    End Function
+
+    Private Function IsWhiteSpace(ByVal c As Char) As Boolean
+        Return Char.IsWhiteSpace(c)
+    End Function
+
+    Private Function IsMulOrDivOperator(ByVal c As Char) As Boolean
+        Return "*/".IndexOf(c) > -1
+    End Function
+
+    Private Function IsAddOrSubOperator(ByVal c As Char) As Boolean
+        Return "+-".IndexOf(c) > -1
+    End Function
+#End Region
+
+#Region "Scanner"
+    Private ReadOnly Property LookAhead() As Char
+        Get
+            Dim result As Char
+            If m_CharPos < m_LineLength Then
+                result = m_ThisLine.Chars(m_CharPos)
+            Else
+                result = " "c
+            End If
+            Return result
+        End Get
+    End Property
+
+    Private ReadOnly Property CurrentToken() As String
+        Get
+            Return m_CurrentTokenBldr.ToString()
+        End Get
+    End Property
+
+    Private ReadOnly Property TokenLength() As Integer
+        Get
+            Return m_CurrentTokenBldr.Length()
+        End Get
+    End Property
+
+    Private ReadOnly Property EndOfLine() As Boolean
+        Get
+            Return m_CharPos >= m_LineLength
+        End Get
+    End Property
+
+    Private Function ScanLine() As Boolean
+        Dim result As Boolean
+        Dim line As String
+
+        line = m_InputStream.ReadLine()
+        If line Is Nothing Then
+            result = False
+        Else
+            ' set up line, line length, 
+            ' increment line counter, 
+            ' and set character position back to 0
+            m_ThisLine = line
+            m_LineLength = m_ThisLine.Length
+            m_linePos += 1
+            m_CharPos = 0
+            result = True
+        End If
+
+        Return result
+    End Function
+
+    Private Sub ScanNumber()
+        m_CurrentTokenBldr = New StringBuilder
+
+        Do While m_CharPos < m_LineLength
+            If Not IsNumeric(LookAhead) Then
+                Exit Do
+            End If
+            m_CurrentTokenBldr.Append(LookAhead)
+            m_CharPos += 1
+        Loop
+    End Sub
+
+    Private Sub SkipWhiteSpace()
+        Do While IsWhiteSpace(LookAhead)
+            If EndOfLine() Then
+                Exit Do
+            Else
+                m_CharPos += 1
+            End If
+        Loop
+    End Sub
+
+    Private Sub ScanMulOrDivOperator()
+        m_CurrentTokenBldr = New StringBuilder
+
+        If IsMulOrDivOperator(LookAhead) Then
+            m_CurrentTokenBldr.Append(LookAhead)
+            m_CharPos += 1
+        End If
+    End Sub
+
+    Private Sub ScanAddOrSubOperator()
+        m_CurrentTokenBldr = New StringBuilder
+
+        If IsAddOrSubOperator(LookAhead) Then
+            m_CurrentTokenBldr.Append(LookAhead)
+            m_CharPos += 1
+        End If
+    End Sub
+
+    Private Sub SkipCharacter()
+        m_CharPos += 1
+    End Sub
+#End Region
+
+#Region "Parser"
+    Private Function ParseLine() As ParseStatus
+        Dim result As ParseStatus
+
+        SkipWhiteSpace()
+
+        result = ParseNumericExpression()
+
+        If result.code = 0 Then
+            If Not EndOfLine() Then
+                result = CreateError(1, "end of statement")
+            Else
+                m_Gen.EmitWriteLine()
+            End If
+        End If
+
+        Return result
+    End Function
+
+    Private Function ParseNumber() As ParseStatus
+        Dim result As ParseStatus
+
+        ' Ask the scanner to read a number
+        ScanNumber()
+
+        If TokenLength = 0 Then
+            result = CreateError(1, "a number")
+        ElseIf TokenLength = 1 AndAlso _
+                Not Char.IsDigit(CurrentToken.Chars(0)) _
+                Then 
+            result = CreateError(1, "a number")
+        Else
+            ' Get the current token, and
+            ' Emit it
+            m_Gen.EmitNumber(CInt(CurrentToken))
+            result = CreateError(0, "Ok")
+        End If
+
+        Return result
+    End Function
+
+    Private Function ParseFactor() As ParseStatus
+        Dim result As ParseStatus
+
+        If LookAhead.Equals("("c) Then
+            SkipCharacter()
+
+            result = ParseNumericExpression()
+
+            If result.Code = 0 Then
+                If Not LookAhead.Equals(")"c) Then
+                    result = CreateError(1, ")")
+                Else
+                    SkipCharacter()
+                End If
+            End If
+        Else
+            result = ParseNumber()
+        End If
+
+        SkipWhiteSpace()
+
+        Return result
+    End Function
+
+    Private Function ParseMulOrDivOperator() As ParseStatus
+        Dim result As ParseStatus
+        Dim currentoperator As String = CurrentToken
+
+        SkipWhiteSpace()
+
+        result = ParseFactor()
+
+        If result.Code = 0 Then
+            If currentoperator = "*" Then
+                m_Gen.EmitMultiply()
+            Else
+                m_Gen.EmitDivide()
+            End If
+        End If
+
+        Return result
+    End Function
+
+    Private Function ParseTerm() As ParseStatus
+        Dim result As ParseStatus
+
+        result = ParseFactor()
+
+        Do While result.Code = 0 _
+            AndAlso _
+            IsMulOrDivOperator(LookAhead)
+
+            ScanMulOrDivOperator()
+
+            If TokenLength = 0 Then
+                result = CreateError(1, "* or /")
+            Else
+                result = ParseMulOrDivOperator()
+                SkipWhiteSpace()
+            End If
+        Loop
+
+        Return result
+    End Function
+
+    Private Function ParseAddOrSubOperator() As ParseStatus
+        Dim result As ParseStatus
+        Dim currentoperator As String = CurrentToken
+
+        SkipWhiteSpace()
+
+        result = ParseTerm()
+
+        If result.Code = 0 Then
+            If currentoperator = "+" Then
+                m_Gen.EmitAdd()
+            Else
+                m_Gen.EmitSubtract()
+            End If
+        End If
+
+        Return result
+    End Function
+
+    Private Function ParseNumericExpression() As ParseStatus
+        Dim result As ParseStatus
+
+        result = ParseTerm()
+
+        Do While result.Code = 0 _
+            AndAlso _
+            IsAddOrSubOperator(LookAhead)
+
+            ScanAddOrSubOperator()
+
+            If TokenLength = 0 Then
+                result = CreateError(1, "+ or -")
+            Else
+                result = ParseAddOrSubOperator()
+                SkipWhiteSpace()
+            End If
+        Loop
+
+        Return result
+    End Function
+#End Region
+
+    Public Function Parse() As ParseStatus
+        Dim result As ParseStatus
+        Do While ScanLine()
+            result = ParseLine()
+            If result.Code <> 0 Then
+                Exit Do
+            End If
+        Loop
+        Return result
+    End Function
+
+    Public Sub New( _
+        ByVal newStream As TextReader, _
+        ByVal newGen As CodeGen _
+        )
+
+        m_InputStream = newStream
+        m_Gen = newGen
+    End Sub
+End Class
